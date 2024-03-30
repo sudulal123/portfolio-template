cbfdbe97aa62955b783784473b44e2dda1b66937
sdulal123 <sushildulal123@gmail.com>
2024-03-30 21:43:50 +0100

    07_Framer_motion_page_transition

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import Navbar from "@/components/navbar";
+import PageTransition from "@/components/pageTransition";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,13 +13,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="w-screen h-screen bg-gradient-to-b from-blue-50 to-red-100">
-            <div className="h-24">
-              <Navbar/>
-            </div>
-          <div className="h-[calc(100vh-6rem)]">{children}</div>
-        </div>
-        </body>
+        <PageTransition>
+            {children}
+        </PageTransition>
+      </body>
     </html>
   );
 }
