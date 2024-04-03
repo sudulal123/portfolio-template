fa212e1cac29dd027157accc7c8089f36b3c1b40
sdulal123 <sushildulal123@gmail.com>
2024-04-03 12:46:38 +0200

    12_Scroll_animation_SVG_Globe_Window "minor change"

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,8 @@
 "use client";
 
 import {useRef} from "react";
-import Image from "next/image";
-import Bigguy from "@/components/bigguy";
 import { motion, useScroll } from "framer-motion";
+import SideAnimation from "@/components/sideAnimation";
 
 const AboutPage = () => {
     const containerRef = useRef();
@@ -228,7 +227,7 @@ const AboutPage = () => {
 
                 {/*SVG AREA*/}
                 <div className="hidden lg:block w-2/3 sticky top-0 z-30 xl:w-1/2">
-                    <Bigguy scrollYProgress={scrollYProgress}/>
+                    <SideAnimation scrollYProgress={scrollYProgress}/>
                 </div>
             </div>
         </motion.div>
