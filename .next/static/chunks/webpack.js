82c3530d6f1b3f85f1cadb1a8dcb2002f0f172da
sdulal123 <sushildulal123@gmail.com>
2024-04-03 21:33:10 +0200

    12_Scroll_animation_SVG_Globe_Window "unfinished"

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "29a333615ec17ca2"; }
+/******/ 		__webpack_require__.h = function() { return "6c76bcf6bf86de79"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/navLink.jsx
+++ src/components/navLink.jsx
@@ -4,13 +4,12 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 const NavLink = ({link}) => {
-
-const pathName = usePathname();
-    return (
-        <Link className={`rounded p-1 ${pathName === link.url && "bg-black text-white"}`} href={link.url}>
-            {link.title}
-        </Link>
-    )  
-};
+    const pathName = usePathname();
+        return (
+            <Link className={`rounded p-1 ${pathName === link.url && "bg-black text-white"}`} href={link.url}>
+                {link.title}
+            </Link>
+        )  
+    };
   
 export default NavLink;
