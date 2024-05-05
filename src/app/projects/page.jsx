db5f0911ec3d2a4d80f417bee6e552f844318ed3
sdulal123 <sushildulal123@gmail.com>
2024-05-05 12:45:47 +0200

    update

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -9,27 +9,27 @@ const items = [
     {
         id: 1,
         color:  "from-slate-600 to-gray-700",
-        title:  "Nike-clone",
-        desc:   "Nike landing page clone, built with React and Tailwind CSS",
-        img:    "/nike_clone.jpg",
-        link:   "https://sushil-clone-nike-landing-page.netlify.app",  
-    },
-    {
-        id: 2,
-        color:  "from-slate-400 to-gray-500",
-        title:  "Nile",
-        desc:   "A single page E-commerce app built with Angular, TypeScript and integrated with Stripe payment",
-        img:    "/nile_ecom.jpg",
-        link:   "https://nile-rvr.netlify.app/",  
-    },
-    {
-        id: 3,
-        color:  "from-slate-600 to-gray-700",
         title:  "Bishop-Blitz",
         desc:   "An online Chess game built with Angular, TypeScript with Stockfish API. It's in work-in-progress stage, however you can still play online.",
         img:    "/bishop_blitz.jpg",
         link:   "https://bishopblitz.netlify.app/",
-    }
+    },
+    {
+        id: 2,
+        color:  "from-slate-600 to-gray-700",
+        title:  "Nike-clone",
+        desc:   "Nike landing page clone, built with React and Tailwind CSS",
+        img:    "/under_construction.jpg",
+        link:   "https://sushil-clone-nike-landing-page.netlify.app",  
+    },
+    {
+        id: 3,
+        color:  "from-slate-400 to-gray-500",
+        title:  "Nile",
+        desc:   "A single page E-commerce app built with Angular, TypeScript and integrated with Stripe payment",
+        img:    "/under_construction.jpg",
+        link:   "https://nile-rvr.netlify.app/",  
+    },
 ]
 
 const ProjectsPage = () => {
@@ -58,7 +58,7 @@ const ProjectsPage = () => {
                                     <h1 className="text-xl font-bold md:text-4xl lg:text-6xl xl:text-8xl">{item.title}</h1>
                                     {/*Image Container*/}
                                     <div className="relative w-80 h-56 md:w-96 md:h-64 lg:w-[500px] lg:h-[350px] xl:w-[600px] xl:h-[420px] rounded-lg overflow-hidden drop-shadow-[0_5px_5px_rgba(0,0,0,0.5)]">
-                                        <Image src={item.img} alt="" fill/>
+                                        <Image priority src={item.img} alt="" fill />
                                     </div>
                                     {/*Description*/}
                                     <p className="w-80 md:w-96 lg:w-[500px] lg:text-lg xl:w-[600px]">{item.desc}</p>
