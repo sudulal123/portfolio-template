930007ec464c0117f50bfeb762bc51c1689dec62
sdulal123 <sushildulal123@gmail.com>
2024-05-09 20:38:58 +0200

    00_Update_website "unfinished"

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -47,20 +47,28 @@ const Homepage = () => {
           
           {/*BUTTONS*/}
           <div className="flex gap-4 w-full justify-center rounded-lg drop-shadow-[0_5px_5px_rgba(0,0,0,0.5)]">
-              <Link href="/about">
-                <motion.button  
-                  className="p-4 rounded-lg ring-1 ring-black bg-gradient-to-b from-slate-400 to-stone-500 text-white"
-                  
-                  whileHover={{ 
-                    scale: [1.1, 1],    
-                }}
-                transition={{
-                    type: "spring", stiffness: 400, damping: 10 
-                }}
+              <motion.svg
+                  initial={{ opacity: 0.2, y: 0 }}
+                  animate={{ opacity: 1, y: "10px" }}
+                  transition={{ repeat: Infinity, duration: 3, ease: "easeInOut" }}
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  width={50}
+                  height={50}
+                  strokeWidth="1"
+                  stroke="#000000"
                 >
-                  Let's dive in
-                </motion.button>
-              </Link>
+                <path d="M12 6V14"/>
+                <path d="M15 11L12 14L9 11"/>
+                <path d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z"/>
+              </motion.svg>
+              <motion.div
+                initial={{ opacity: 0.2, y: 0 }}
+                animate={{ opacity: 1, y: "10px" }}
+                transition={{ repeat: Infinity, duration: 3, ease: "easeInOut" }}
+              >
+                Scroll down
+              </motion.div>
           </div>
         </div>
     </div>
